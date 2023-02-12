60772a2cc0f0853142a778740261b98fb53ce1b4
hb04380 <49056883+hb04380@users.noreply.github.com>
2023-02-12 18:23:55 +0500

    Update algos.py

--- algos.py
+++ algos.py
@@ -1,19 +1,63 @@
+import random
 def algo_A(n: int):
     ''' Runs in \Theta(n^2) time
     '''
-    pass
+    random_number = random.randint(1, n*100)
+    sum = 0
+    for i in range(n):
+        for j in range(n):
+            sum += j
+            if sum == random_number:
+                return True
+            
+    return False
 
 def algo_B(n: int):
-    ''' Runs in \Theta(n^2) time
-    '''
-    pass
+    
+    check = random.randint(1, n*10)
+    a = 0
+    while (a <= n**2):
+        if (check == a):
+            return a
+        a += 1
+        
+    return a
+    
 
 def algo_C(n: int):
-    ''' Runs in \Theta(n \lg n) time
-    '''
-    pass
+    
+    array = []
+    
+    for i in range (n):
+        array.append(random.randint(1, n*10))
+        
+    low = 0
+    high = len(array) - 1
+    
+    while low <= high:
+        mid = (low + high) // 2
+        if array[mid] == n:
+            return mid
+        elif array[mid] < n:
+            low = mid + 1
+        else:
+            high = mid - 1
+    
+    return -1
+
 
 def algo_D(n: int):
-    ''' Runs in \Theta(n \lg n) time
-    '''
-    pass
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+    
+    f = [0] * (n + 1)
+    f[1] = 1
+    f[2] = 1
+    for i in range(3, n + 1):
+        f[i] = f[i - 1] + f[i - 2]
+    
+    return f[n]
